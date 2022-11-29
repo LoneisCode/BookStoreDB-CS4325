fa76fd798f5035cab90e2d7211ecf82fba782538
johnSoloQ <workmanjohn@me.com>
2022-11-28 23:59:08 -0600

    Admin Portal

--- BooksGalore/AdminPortal.aspx
+++ BooksGalore/AdminPortal.aspx
@@ -1,6 +1,14 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdminPortal.aspx.cs" Inherits="BooksGalore.AdminPortal" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1>
-        ADMINPORTAL
+        ADMIN PORTAL
     </h1>
+    <asp:Button ID="btnCustomerList" runat="server" Text="Customer List" onClick ="btnCustomerList_Click"/>
+    <asp:Button ID="btnOrderList" runat="server" Text="Order List" onClick ="btnOrderList_Click" />
+    <asp:Button ID="btnBookList" runat="server" Text="Book List" onClick ="btnBookList_Click"/>
+    <asp:Button ID="btnSupplierList" runat="server" Text="Supplier List" onClick ="btnSupplierList_Click"/>
+    <asp:Button ID="btnAuthorList" runat="server" Text="Author List" onClick ="btnAuthorList_Click"/>
+
+    <br />
+    <asp:GridView ID="gvAdminInfo" runat="server"></asp:GridView>
 </asp:Content>
--- BooksGalore/AdminPortal.aspx.cs
+++ BooksGalore/AdminPortal.aspx.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using static BooksGalore.CommonQuerys;
 
 namespace BooksGalore
 {
     public partial class AdminPortal : System.Web.UI.Page
     {
+        public static String AdminTableStr;
+        public static String IDName;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
                 if (Session["username"] != null)
                 {
-
+                    gvAdminInfo.Visible = false;
+                    AdminTableStr = "";
+                    IDName = "";
                 }
                 else
                 {
@@ -23,6 +32,233 @@ namespace BooksGalore
                 }
 
             }
+        }
+
+        void CustomerBind()
+        {
+            AdminTableStr = "Customers";
+            IDName = "CustomerID";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["BooksGaloreConnStr"].ConnectionString;
+
+            // 2. Create a SqlCommand object
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Customers";
+            cmd.Connection = conn;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            sda.Fill(dt);
+
+            gvAdminInfo.DataSource = dt;
+            gvAdminInfo.DataBind();
+            conn.Close();
+            gvAdminInfo.Visible = true;
+        }
+
+        void OrderBind()
+        {
+            AdminTableStr = "Orders";
+            IDName = "OrderID";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["BooksGaloreConnStr"].ConnectionString;
+
+            // 2. Create a SqlCommand object
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Orders";
+            cmd.Connection = conn;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            sda.Fill(dt);
+
+            gvAdminInfo.DataSource = dt;
+            gvAdminInfo.DataBind();
+            conn.Close();
+
+            gvAdminInfo.Visible = true;
+        }
+
+        void BookBind()
+        {
+            AdminTableStr = "Books";
+            IDName = "ISBN";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["BooksGaloreConnStr"].ConnectionString;
+
+            // 2. Create a SqlCommand object
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Books";
+            cmd.Connection = conn;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            sda.Fill(dt);
+
+            gvAdminInfo.DataSource = dt;
+            gvAdminInfo.DataBind();
+            conn.Close();
+
+            gvAdminInfo.Visible = true;
+        }
+
+        void SupplierBind()
+        {
+            IDName = "SID";
+            AdminTableStr = "Suppliers";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["BooksGaloreConnStr"].ConnectionString;
+
+            // 2. Create a SqlCommand object
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Suppliers";
+            cmd.Connection = conn;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            sda.Fill(dt);
+
+            gvAdminInfo.DataSource = dt;
+            gvAdminInfo.DataBind();
+            conn.Close();
+
+            gvAdminInfo.Visible = true;
+        }
+
+        void AuthorBind()
+        {
+            IDName = "AuthorID";
+            AdminTableStr = "Authors";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["BooksGaloreConnStr"].ConnectionString;
+
+            // 2. Create a SqlCommand object
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Authors";
+            cmd.Connection = conn;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            sda.Fill(dt);
+
+            gvAdminInfo.DataSource = dt;
+            gvAdminInfo.DataBind();
+            conn.Close();
+
+            gvAdminInfo.Visible = true;
+        }
+
+        protected void gvAdminInfo_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            //edit and delete button
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                Button editButton = e.Row.FindControl("btnEdit") as Button;
+                Button deleteButton = e.Row.FindControl("btnDelete") as Button;
+
+                editButton.CommandArgument = e.Row.Cells[0].Text;
+                deleteButton.CommandArgument = e.Row.Cells[0].Text;
+
+            }
+        }
+
+        protected void gvAdminInfo_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            //edit adn delete button functionality
+            int ID = 0;
+            if (e.CommandName == "EditRow") // the Edit button was clicked
+            {
+                ID = int.Parse(e.CommandArgument.ToString());
+
+                if (AdminTableStr == "Customers")
+                    Response.Redirect("EditCustomer.aspx");
+                else if (AdminTableStr == "Orders")
+                    Response.Redirect("AdminPortal.aspx");
+                else if (AdminTableStr == "Books")
+                    Response.Redirect("EditBook.aspx");
+                else if (AdminTableStr == "Suppliers")
+                    Response.Redirect("AdminPortal.aspx");
+                else
+                    Response.Redirect("EditAuthor.aspx");
+
+            }
+            else if (e.CommandName == "DeleteRow") // the Delete button was clicked
+            {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = WebConfigurationManager.ConnectionStrings["BooksGaloreConnStr"].ConnectionString;
+                    ID = int.Parse(e.CommandArgument.ToString());
+                    // 2. Create a SqlCommand object
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = $"DELETE FROM {AdminTableStr} WHERE {IDName} = " + ID;
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    if (AdminTableStr == "Customers")
+                        CustomerBind();
+                    else if (AdminTableStr == "Orders")
+                        OrderBind();
+                    else if (AdminTableStr == "Books")
+                        BookBind();
+                    else if (AdminTableStr == "Suppliers")
+                        SupplierBind();
+                    else
+                        AuthorBind();
+
+                }
+            }
+        }
+
+        protected void btnCustomerList_Click(object sender, EventArgs e)
+        {
+            CustomerBind();
+        }
+
+        protected void btnOrderList_Click(object sender, EventArgs e)
+        {
+            OrderBind();
+        }
+
+        protected void btnBookList_Click(object sender, EventArgs e)
+        {
+            BookBind();
+        }
+
+        protected void btnSupplierList_Click(object sender, EventArgs e)
+        {
+            SupplierBind();
+        }
+
+        protected void btnAuthorList_Click(object sender, EventArgs e)
+        {
+            AuthorBind();
         }
     }
 }
